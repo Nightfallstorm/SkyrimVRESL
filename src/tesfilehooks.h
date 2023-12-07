71a502e42c4258fc56194b796bbdcd2c25adc317
nightfallstorm <109926124+Nightfallstorm@users.noreply.github.com>
2023-12-06 23:53:13 -0500

    Add TESFile hooks

--- src/tesfilehooks.h
+++ src/tesfilehooks.h
@@ -4,9 +4,117 @@
 
 namespace tesfilehooks
 {
+	static bool HasFile(RE::BSTArray<RE::TESFile*> a_array, RE::TESFile* a_file)
+	{
+		for (auto file : a_array) {
+			if (file == a_file) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Reinterpret datahandler as our custom datahandler with ESL support
+	// and add the file with the correct compile indexing
+	// This is a replica of what Skyrim SE does when adding files
+	static void AddFile(RE::TESFile* a_file)
+	{
+		logger::info("AddFile invoked {} {:x}", std::string(a_file->fileName), a_file->compileIndex);
+		DataHandlerSE* handler = reinterpret_cast<DataHandlerSE*>(RE::TESDataHandler::GetSingleton());
+		auto& fileCollection = handler->compiledFileCollection;
+		if (!a_file->IsLight() && HasFile(fileCollection.files, a_file)) {
+			return;
+		} else if (a_file->IsLight() && HasFile(fileCollection.smallFiles, a_file)) {
+			return;
+		}
+
+		if (a_file->IsLight()) {
+			fileCollection.smallFiles.push_back(a_file);
+			auto smallFileCompileIndex = fileCollection.smallFiles.size() - 1;
+			a_file->flags &= 0xFFFFFFu;
+			a_file->flags |= 0xFE << 24;
+			a_file->compileIndex = 0xFE;
+			a_file->smallFileCompileIndex = smallFileCompileIndex;
+		} else {
+			fileCollection.files.push_back(a_file);
+			auto fileCompileIndex = fileCollection.files.size() - 1;
+			a_file->flags &= 0xFFFFFFu;
+			a_file->flags |= fileCompileIndex << 24;
+			a_file->compileIndex = fileCompileIndex;
+			a_file->smallFileCompileIndex = 0;
+		}
+		logger::info("AddFile finished");
+	}
+
 	struct AddTESFileHook
 	{
-		static bool HasFile(RE::BSTArray<RE::TESFile*> a_array, RE::TESFile* a_file) {
+		static void AddFileThunk(DataHandlerSE* a_handler, RE::TESFile* a_file) {
+			return AddFile(a_file);			
+		}
+
+		static void Install()
+		{
+			REL::Relocation<std::uintptr_t> target{ REL::Offset(0x17DFF0) };
+
+			auto& trampoline = SKSE::GetTrampoline();
+			SKSE::AllocTrampoline(14);
+			trampoline.write_branch<5>(target.address(), AddFileThunk);
+			logger::info("Install AddFile hook at {:x}", target.address());
+			logger::info("Install AddFile hook at offset {:x}", target.offset());
+		}
+	};
+
+	struct AddTESFileHook1
+	{
+		struct TrampolineCall : Xbyak::CodeGenerator
+		{
+			// Call AddFile method, then jump back to rest of code
+			TrampolineCall(std::uintptr_t jmpAfterCall, std::uintptr_t func = stl::unrestricted_cast<std::uintptr_t>(AddFile))
+			{
+				Xbyak::Label funcLabel;
+				sub(rsp, 0x20);
+				call(ptr[rip + funcLabel]);
+				add(rsp, 0x20);
+				mov(rcx, jmpAfterCall);
+				jmp(rcx);
+				L(funcLabel);
+				dq(func);
+			}
+		};
+
+		static void Install()
+		{
+			REL::Relocation<std::uintptr_t> target{ REL::Offset(0x1831F0 + 0x807) };
+			REL::Relocation<std::uintptr_t> jmp{ REL::Offset(0x1831F0 + 0x852) };
+			REL::safe_fill(target.address(), REL::NOP, jmp.address() - target.address());
+
+			auto trampolineJmp = TrampolineCall(jmp.address());
+			REL::safe_write(target.address(), trampolineJmp.getCode(), trampolineJmp.getSize());
+
+			logger::info("Install AddFile1 hook at address {:x}", target.address());
+			logger::info("Install AddFile1 hook at offset {:x}", target.offset());
+		}
+	};
+
+	struct AddTESFileHook2
+	{
+		struct TrampolineCall : Xbyak::CodeGenerator
+		{
+			// Call AddFile method, then jump back to rest of code
+			TrampolineCall(std::uintptr_t jmpAfterCall, std::uintptr_t func = stl::unrestricted_cast<std::uintptr_t>(AddFile))
+			{
+				Xbyak::Label funcLabel;
+				sub(rsp, 0x20);
+				call(ptr[rip + funcLabel]);
+				add(rsp, 0x20);
+				mov(rcx, jmpAfterCall);
+				jmp(rcx);
+				L(funcLabel);
+				dq(func);
+			}
+		};
+		static bool HasFile(RE::BSTArray<RE::TESFile*> a_array, RE::TESFile* a_file)
+		{
 			for (auto file : a_array) {
 				if (file == a_file) {
 					return true;
@@ -17,8 +125,10 @@ namespace tesfilehooks
 		// Reinterpret datahandler as our custom datahandler with ESL support
 		// and add the file with the correct compile indexing
 		// This is a replica of what Skyrim SE does when adding files
-		static void AddFile(DataHandlerSE* a_handler, RE::TESFile* a_file) {
+		static void AddFile(RE::TESFile* a_file)
+		{
 			logger::info("AddFile called");
+			DataHandlerSE* a_handler = reinterpret_cast<DataHandlerSE*>(RE::TESDataHandler::GetSingleton());
 			auto& fileCollection = a_handler->compiledFileCollection;
 			if (!a_file->IsLight() && HasFile(fileCollection.files, a_file)) {
 				return;
@@ -43,16 +153,197 @@ namespace tesfilehooks
 			}
 		}
 
-		static void Install() {
-			REL::Relocation<std::uintptr_t> target{ REL::Offset(0x17DFF0) };
+		static void Install()
+		{
+			REL::Relocation<std::uintptr_t> target{ REL::Offset(0x1831F0 + 0x807) };
+			REL::Relocation<std::uintptr_t> jmp{ REL::Offset(0x1831F0 + 0x852) };
+			REL::safe_fill(target.address(), REL::NOP, jmp.address() - target.address());
 
-			auto& trampoline = SKSE::GetTrampoline();
-			SKSE::AllocTrampoline(14);
-			trampoline.write_branch<5>(target.address(), AddFile);
-			logger::info("Install AddFile hook at {:x}", target.address());
-			logger::info("Install AddFile hook at offset {:x}", target.offset());
+			auto trampolineJmp = TrampolineCall(jmp.address());
+			REL::safe_write(target.address(), trampolineJmp.getCode(), trampolineJmp.getSize());
+
+			logger::info("Install AddFile1 hook at address {:x}", target.address());
+			logger::info("Install AddFile1 hook at offset {:x}", target.offset());
 		}
 	};
+
+	struct LoadFilesHook {
+		static inline REL::Relocation<std::uintptr_t> target{ REL::Offset(0x17EFF0) };
+
+		static inline REL::Relocation<int> iTotalForms{ REL::Offset(0x1F889B4) };
+
+		struct TrampolineCall : Xbyak::CodeGenerator
+		{
+			// Call AddFile method, then jump back to rest of code
+			TrampolineCall(std::uintptr_t jmpAfterCall, std::uintptr_t func = stl::unrestricted_cast<std::uintptr_t>(AddFile))
+			{
+				Xbyak::Label funcLabel;
+				mov(rcx, rbx);
+				sub(rsp, 0x20);
+				call(ptr[rip + funcLabel]);
+				add(rsp, 0x20);
+				mov(rcx, jmpAfterCall);
+				jmp(rcx);
+				L(funcLabel);
+				dq(func);
+			}
+		};
+
+		struct TrampolineCall2 : Xbyak::CodeGenerator
+		{
+			// Call AddFile method, then jump back to rest of code
+			TrampolineCall2(std::uintptr_t jmpAfterCall, std::uintptr_t func = stl::unrestricted_cast<std::uintptr_t>(AddFile))
+			{
+				Xbyak::Label funcLabel;
+				sub(rsp, 0x20);
+				call(ptr[rip + funcLabel]);
+				add(rsp, 0x20);
+				mov(rcx, jmpAfterCall);
+				jmp(rcx);
+				L(funcLabel);
+				dq(func);
+			}
+		};
+
+		// Hook where TESDataHandler->loadedMods is used, and replace with the ESL/ESP split
+
+		static void EraseLoadedModCountReset() {
+			REL::safe_fill(target.address() + 0x68, REL::NOP, 0x3);
+			logger::info("Erased LoadedModCountReset");
+		}
+
+		static void InstallAddFile() {
+			std::uintptr_t start = target.address() + 0x196;
+			std::uintptr_t end = target.address() + 0x1B9;
+			REL::safe_fill(start, REL::NOP, end - start);
+
+			auto trampolineJmp = TrampolineCall(end);
+
+			auto& trampoline = SKSE::GetTrampoline();
+			SKSE::AllocTrampoline(trampolineJmp.getSize());
+			auto result = trampoline.allocate(trampolineJmp);
+			auto& trampoline2 = SKSE::GetTrampoline();
+			SKSE::AllocTrampoline(14);
+			trampoline2.write_branch<5>(start, (std::uintptr_t)result);
+			
+			//REL::safe_write(start, trampolineJmp.getCode(), trampolineJmp.getSize());
+			//if (trampolineJmp.getSize() > end - start) {
+			//	logger::critical("InstallAddFile trampoline too big by {} bytes!", trampolineJmp.getSize() - (end - start));
+			//}
+
+			logger::info("Install LoadFilesHook hook at address {:x}", start);
+		}
+
+		static void InstallAddFile2()
+		{
+			std::uintptr_t start = target.address() + 0x1F0;
+			std::uintptr_t end = target.address() + 0x217;
+			REL::safe_fill(start, REL::NOP, end - start);
+
+			auto trampolineJmp = TrampolineCall2(end);
+			REL::safe_write(start, trampolineJmp.getCode(), trampolineJmp.getSize());
+
+			if (trampolineJmp.getSize() > end - start) {
+				logger::critical("InstallAddFile2 trampoline too big by {} bytes!", trampolineJmp.getSize() - (end - start));
+			}
+
+			logger::info("Install LoadFilesHook2 hook at address {:x}", start);
+		}
+
+		static void OpenTESLoopThunk() {
+			logger::info("OpenTESLoop invoked!");
+			// Replica of what SE does, but for VR
+			auto handler = reinterpret_cast<DataHandlerSE*>(RE::TESDataHandler::GetSingleton());
+			int* totalForms = reinterpret_cast<int*>(iTotalForms.address());
+			for (auto file : handler->compiledFileCollection.files) {
+				logger::info("OpenTESLoop opening file! {} {:x}", std::string(file->fileName), file->compileIndex);
+				file->OpenTES(RE::NiFile::OpenMode::kReadOnly, 0);
+				*totalForms += file->unk430;
+			}
+			for (auto file : handler->compiledFileCollection.smallFiles) {
+				logger::info("OpenTESLoop opening small file! {} {:x}", std::string(file->fileName), file->compileIndex);
+				file->OpenTES(RE::NiFile::OpenMode::kReadOnly, 0);
+				*totalForms += file->unk430;
+			}
+			logger::info("OpenTESLoop finished!");
+		}
+
+		static void InstallOpenTESLoop()
+		{
+			std::uintptr_t start = target.address() + 0x21D;
+			std::uintptr_t end = target.address() + 0x266;
+			REL::safe_fill(start, REL::NOP, end - target.address());
+
+			auto trampolineJmp = TrampolineCall2(end, stl::unrestricted_cast<std::uintptr_t>(OpenTESLoopThunk));
+			REL::safe_write(start, trampolineJmp.getCode(), trampolineJmp.getSize());
+
+			
+			if (trampolineJmp.getSize() > end - start) {
+				logger::critical("InstallOpenTESLoop trampoline too big by {} bytes!", trampolineJmp.getSize() - (end - start));
+			}
+
+			logger::info("Install LoadFilesHook3 hook at address {:x}", start);
+		}
+
+		static void ConstructObjectList(DataHandlerSE* a_handler, RE::TESFile* a_file, bool a_isFirstPlugin)
+		{
+			using func_t = decltype(&ConstructObjectList);
+			REL::Relocation<func_t> func{ REL::Offset(0x180870) };
+			return func(a_handler, a_file, a_isFirstPlugin);
+		}
+
+		static void ConstructObjectListThunk()
+		{
+			logger::info("ConstructObjectListThunk invoked!");
+			// Replica of what SE does, but for VR
+			auto handler = reinterpret_cast<DataHandlerSE*>(RE::TESDataHandler::GetSingleton());
+			bool firstPlugin = true;
+			logger::info("ConstructObjectListThunk 1");
+			for (auto file : handler->files) {
+				logger::info("ConstructObjectListThunk 2");
+				if (file->compileIndex != 0xFF && file != handler->activeFile) {
+					logger::info("ConstructObjectListThunk 3 {} {:x}", std::string(file->fileName), file->compileIndex);
+					ConstructObjectList(handler, file, firstPlugin);
+					logger::info("ConstructObjectListThunk 4");
+					firstPlugin = false;
+					logger::info("ConstructObjectListThunk 5");
+				}
+			}
+			logger::info("ConstructObjectListThunk 6");
+			if (handler->activeFile) {
+				logger::info("ConstructObjectListThunk 7");
+				ConstructObjectList(handler, handler->activeFile, firstPlugin);
+				logger::info("ConstructObjectListThunk 8");
+				firstPlugin = false;
+				logger::info("ConstructObjectListThunk 9");
+			}
+			logger::info("ConstructObjectListThunk finished!");
+		}
+
+		static void InstallConstructObjectListLoop() {
+			std::uintptr_t start = target.address() + 0x29C;
+			std::uintptr_t end = target.address() + 0x2D2;
+			REL::safe_fill(start, REL::NOP, end - target.address());
+
+			auto trampolineJmp = TrampolineCall2(end, stl::unrestricted_cast<std::uintptr_t>(ConstructObjectListThunk));
+			REL::safe_write(start, trampolineJmp.getCode(), trampolineJmp.getSize());
+
+			if (trampolineJmp.getSize() > end - start) {
+				logger::critical("InstallConstructObjectListLoop trampoline too big by {} bytes!", trampolineJmp.getSize() - (end - start));
+			}
+
+			logger::info("Install LoadFilesHook4 hook at address {:x}", start);
+		}
+
+		static void Install() {
+			EraseLoadedModCountReset();		
+			InstallAddFile();
+			InstallAddFile2();
+			InstallOpenTESLoop();
+			InstallConstructObjectListLoop();
+		}
+	};
+
 	struct DuplicateHook
 	{
 		// Copy over the smallCompileIndex, which is padding in VR normally
@@ -90,5 +381,7 @@ namespace tesfilehooks
 	{
 		DuplicateHook::Install();
 		AddTESFileHook::Install();
+		AddTESFileHook1::Install();
+		LoadFilesHook::Install();
 	}
 }
