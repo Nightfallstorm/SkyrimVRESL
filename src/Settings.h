d0fdc858733cffdc2cf27b068b384303fac44dd0
nightfallstorm <109926124+Nightfallstorm@users.noreply.github.com>
2023-12-13 22:39:23 -0500

    Added settings
    Changed some logs to debug to reduce log spam by default

---
+++ src/Settings.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Util.h"
+
+class Settings
+{
+public:
+	[[nodiscard]] static Settings* GetSingleton();
+
+	void Load();
+
+	struct SettingValues
+	{
+		void Load(CSimpleIniA& a_ini);
+
+		spdlog::level::level_enum logLevel{ spdlog::level::level_enum::info };
+		spdlog::level::level_enum flushLevel{ spdlog::level::level_enum::trace };
+	} settings;
+
+private:
+	struct detail
+	{
+		template <class T>
+		static void get_value(CSimpleIniA& a_ini, T& a_value, const char* a_section, const char* a_key, const char* a_comment)
+		{
+			if constexpr (std::is_same_v<bool, T>) {
+				a_value = a_ini.GetBoolValue(a_section, a_key, a_value);
+				a_ini.SetBoolValue(a_section, a_key, a_value, a_comment);
+
+				GetSingleton()->settingsMap.emplace(a_key, a_value);
+			} else if constexpr (std::is_floating_point_v<T>) {
+				a_value = static_cast<T>(a_ini.GetDoubleValue(a_section, a_key, a_value));
+				a_ini.SetDoubleValue(a_section, a_key, a_value, a_comment);
+
+				GetSingleton()->settingsMap.emplace(a_key, a_value != 1.0);
+			} else if constexpr (std::is_arithmetic_v<T> || std::is_enum_v<T>) {
+				a_value = Util::stl::string::lexical_cast<T>(a_ini.GetValue(a_section, a_key, std::to_string(a_value).c_str()));
+				a_ini.SetValue(a_section, a_key, std::to_string(a_value).c_str(), a_comment);
+
+				GetSingleton()->settingsMap.emplace(a_key, a_value != 0);
+			} else {
+				a_value = a_ini.GetValue(a_section, a_key, a_value.c_str());
+				a_ini.SetValue(a_section, a_key, a_value.c_str(), a_comment);
+			}
+		}
+	};
+
+	robin_hood::unordered_flat_map<std::string, bool> settingsMap{};
+};
